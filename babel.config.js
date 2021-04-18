1d0db66b53513853076cb9b96caa0d2a5efffede
神代綺凛 <i@loli.best>
2021-04-19 01:48:07 +0800

    chore: publish to npm

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  plugins: ['@babel/plugin-proposal-nullish-coalescing-operator', '@babel/plugin-proposal-optional-chaining'],
-  presets: ['@babel/preset-env'],
+  plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+  ],
+  ...(process.env.NODE_ENV === 'test'
+    ? {
+        presets: ['@babel/preset-env'],
+      }
+    : {
+        ignore: [/__mocks__/],
+      }),
 };
