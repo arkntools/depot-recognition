05156c6bd682152c59af29b769733450c53e871e
神代綺凛 <i@loli.best>
2021-04-29 18:02:57 +0800

    feat: build cjs and esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import { babel } from '@rollup/plugin-babel';
+import { babel, getBabelOutputPlugin } from '@rollup/plugin-babel';
 import externalGlobals from 'rollup-plugin-external-globals';
 import copy from 'rollup-plugin-copy';
 import del from 'rollup-plugin-delete';
@@ -7,11 +7,18 @@ const { ROLLUP_WATCH, CI } = process.env;
 
 const getConfig = (plugins = []) => ({
   external: [/^lodash/, /^@arkntools/, 'jimp', 'jszip', 'simple-statistics'],
-  output: {
-    dir: 'dist',
-    format: 'esm',
-  },
-  plugins: [babel({ babelHelpers: 'bundled' }), ...plugins.filter(v => v)],
+  plugins: [...plugins.filter(v => v), babel({ babelHelpers: 'bundled' })],
+  output: [
+    {
+      dir: 'dist',
+      format: 'esm',
+      plugins: [getBabelOutputPlugin({ plugins: ['@babel/plugin-transform-modules-commonjs'] })],
+    },
+    {
+      dir: 'dist/es',
+      format: 'esm',
+    },
+  ],
 });
 
 export default [
