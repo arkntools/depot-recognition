13f1cedd616cded3b95817621b2206e50ef2d6b3
神代綺凛 <i@loli.best>
2021-12-19 20:06:41 +0800

    chore: update scripts

--- src/@types/ocrad.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '@arkntools/scripts/dist/ocrad' {
-  export default function (image: ImageData, options?: { numeric: boolean }): string;
-}
--- src/worker/importScripts.ts
+++ src/worker/importScripts.ts
@@ -3,5 +3,5 @@ self.importScripts(
   'https://cdn.jsdelivr.net/npm/simple-statistics@7.7.0/dist/simple-statistics.min.js',
   'https://cdn.jsdelivr.net/npm/jszip@3.7.1/dist/jszip.min.js',
   'https://cdn.jsdelivr.net/npm/@arkntools/scripts@1.0.2/dist/ocrad.js',
-  'https://cdn.jsdelivr.net/npm/@arkntools/scripts@1.0.2/dist/jimp.js',
+  'https://cdn.jsdelivr.net/npm/@arkntools/scripts@1.1.1/dist/jimp4worker.js',
 );
