ff8ec26724c7149aa9e1172b9ba42e6080c8a62a
神代綺凛 <i@loli.best>
2022-05-15 16:36:26 +0800

    chore: update dependencies

--- src/worker/importScripts.ts
+++ src/worker/importScripts.ts
@@ -1,7 +1,7 @@
 self.importScripts(
   'https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js',
-  'https://cdn.jsdelivr.net/npm/simple-statistics@7.7.0/dist/simple-statistics.min.js',
-  'https://cdn.jsdelivr.net/npm/jszip@3.7.1/dist/jszip.min.js',
+  'https://cdn.jsdelivr.net/npm/simple-statistics@7.7.5/dist/simple-statistics.min.js',
+  'https://cdn.jsdelivr.net/npm/jszip@3.9.1/dist/jszip.min.js',
   'https://cdn.jsdelivr.net/npm/@arkntools/scripts@1.0.2/dist/ocrad.js',
   'https://cdn.jsdelivr.net/npm/@arkntools/scripts@1.1.1/dist/jimp4worker.js',
 );
