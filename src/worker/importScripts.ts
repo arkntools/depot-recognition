85a59781f89178714cd1490e2f413b30867beaba
神代綺凛 <i@loli.best>
2022-08-15 16:02:32 +0800

    feat: change CDN

--- src/worker/importScripts.ts
+++ src/worker/importScripts.ts
@@ -1,7 +1,7 @@
 self.importScripts(
-  'https://unpkg.com/lodash@4.17.21/lodash.min.js',
-  'https://unpkg.com/simple-statistics@7.7.5/dist/simple-statistics.min.js',
-  'https://unpkg.com/jszip@3.10.0/dist/jszip.min.js',
-  'https://unpkg.com/@arkntools/scripts@1.1.1/dist/ocrad.js',
-  'https://unpkg.com/@arkntools/scripts@1.1.1/dist/jimp4worker.js',
+  'https://fastly.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js',
+  'https://fastly.jsdelivr.net/npm/simple-statistics@7.7.6/dist/simple-statistics.min.js',
+  'https://fastly.jsdelivr.net/npm/jszip@3.10.1/dist/jszip.min.js',
+  'https://fastly.jsdelivr.net/npm/@arkntools/scripts@1.1.1/dist/ocrad.js',
+  'https://fastly.jsdelivr.net/npm/@arkntools/scripts@1.1.1/dist/jimp4worker.js',
 );
